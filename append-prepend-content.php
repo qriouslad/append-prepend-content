7a6b9e6219fed83209b46a6a5104d365af14ffb6
Bowo <wibowosulistio@gmail.com>
2019-03-11 09:30:51 +0700

    Add filter to prepend or append content added in the settings

--- append-prepend-content.php
+++ append-prepend-content.php
@@ -24,10 +24,32 @@ class AppOrPrepp
 	}
 
 	public function __construct() {
+
 		if ( is_admin() ) {
 			include_once( plugin_dir_path( __FILE__ ) . '/admin.php' );
 			new AppOrPrepp_Admin();
 		}
+
+		add_filter( 'the_content', array( $this, 'apporprepp_the_content' ) );
+
+	}
+
+	public function apporprepp_the_content ( $content ) {
+		$post = get_post();
+		$post_type = get_post_type( $post );
+		$prepend = get_option( 'prepend_' . $post_type, '' );
+		$append = get_option( 'append_' . $post_type, '' );
+
+		if ( $prepend ) {
+			$content = wpautop( $prepend ) . $content;
+		}
+
+		if ( $append ) {
+			$content = $content . wpautop( $append );
+		}
+
+		return $content;
+
 	}
 
 }
